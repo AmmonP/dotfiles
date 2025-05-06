58e0ab8bd9c4d606814d591d842fd0a8a3473ebb
Ammon P <latsu12@gmail.com>
2025-05-06 11:04:15 -0600

    Assume that ffmpeg is in PATH

--- scripts/compressVideo.sh
+++ scripts/compressVideo.sh
@@ -10,7 +10,7 @@
 INPUT_PATH="${1}"
 FILE_NAME="${INPUT_PATH%.*}"
 OUPUT_PATH="${FILE_NAME}-compressed.mp4"
-FFMPEG="/usr/local/bin/ffmpeg"
+FFMPEG="ffmpeg"
 
 "${FFMPEG}" "-i" "${INPUT_PATH}" "-c:v" "libx265" "-preset" "veryslow" "${OUPUT_PATH}"
 exit
